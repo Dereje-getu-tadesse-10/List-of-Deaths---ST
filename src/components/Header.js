043678de474a0346ce5209c07360a69f77a003a2
Dereje <getutadessedereje@icloud.com>
2022-07-06 17:49:52 +0200

    Update: add style header

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,6 +4,17 @@ const Header = () => {
   return (
     <header>
         <h1 className="title-red"> ST </h1>
+
+        <nav>
+            <ul>
+                <li>
+                    <a href="/">Github</a>
+                </li>
+                <li>
+                    <a href="https://strangerthings.fandom.com/wiki/List_of_Deaths">Stranger things fandom</a>
+                </li>
+            </ul>
+        </nav>
     </header>
 
   )
