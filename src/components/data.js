d893d78c574272ff15ca02ea246b21c04de26ee9
Dereje <getutadessedereje@icloud.com>
2022-07-06 18:16:26 +0200

    Update: add data

---
+++ src/components/data.js
@@ -0,0 +1,44 @@
+const data = [
+    {
+        id: 1,
+        photo: 'https://static.wikia.nocookie.net/strangerthings8338/images/8/84/FredBenson.png/revision/latest/scale-to-width-down/350?cb=20220529133228',
+        victime: 'Fred Benson',
+        episode: 'Chapter Two: Vecna\'s Curse',
+        causeOfDeath: 'Bones snapped and eyes gouged out',
+        killer: 'Vecna',
+    },
+    {
+        id: 2,
+        photo: 'https://static.wikia.nocookie.net/strangerthings8338/images/d/d1/Chrissy_ST4.jpeg/revision/latest/scale-to-width-down/350?cb=20220608210406',
+        victime: 'Chrissy Cunningham',
+        episode: 'Chapter One: The Hellfire Club',
+        causeOfDeath: 'Bones snapped and eyes gouged out ',
+        killer: 'Vecna',
+    },
+    {
+        id: 3,
+        photo: 'https://static.wikia.nocookie.net/strangerthings8338/images/9/99/Patrickstrangerthings.png/revision/latest/scale-to-width-down/350?cb=20220602230200',
+        victime: 'Patrick McKinney',
+        episode: 'Chapter Five: The Nina Project',
+        causeOfDeath: 'Bones snapped and eyes gouged out',
+        killer: 'Vecna',
+    },
+    {
+        id: 4,
+        photo: 'https://static.wikia.nocookie.net/strangerthings8338/images/b/b5/Brenner_-_The_Nina_Project.png/revision/latest/scale-to-width-down/350?cb=20220606135528',
+        victime: 'Martin Brenner',
+        episode: 'Chapter Eight: Papa ',
+        causeOfDeath: 'Shot multiple times',
+        killer: 'Unnamed Military Soldier',
+    },
+    {
+        id: 5,
+        photo: 'https://static.wikia.nocookie.net/strangerthings8338/images/c/ca/Eddie_Munson.jpg/revision/latest/scale-to-width-down/350?cb=20220608205857',
+        victime: 'Eddie Munson',
+        episode: 'Chapter Nine: The Piggyback',
+        causeOfDeath: 'Attacked and bitten',
+        killer: 'Demobats',
+    }
+]
+
+export default data;
